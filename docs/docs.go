983228227e61b2c7a44c6e35174cd71e433b44e0
ryan <ryan@arctel.net>
2026-06-10 22:36:17 +0800

    AccessToken 默认非管理员权限

--- docs/docs.go
+++ docs/docs.go
@@ -3641,7 +3641,7 @@ const docTemplate = `{
                         "SessionCookie": []
                     }
                 ],
-                "description": "为当前用户新建一个 API 访问令牌，仅在此接口返回一次明文令牌值，请妥善保存。",
+                "description": "为当前用户新建一个 API 访问令牌，仅在此接口返回一次明文令牌值，请妥善保存。可通过 is_admin 字段赋予令牌管理员权限（仅管理员用户可设置）。",
                 "consumes": [
                     "application/json"
                 ],
@@ -4555,6 +4555,9 @@ const docTemplate = `{
                 },
                 "id": {
                     "type": "integer"
+                },
+                "is_admin": {
+                    "type": "boolean"
                 },
                 "last_used_at": {
                     "type": "string"
@@ -5496,6 +5499,9 @@ const docTemplate = `{
         "user.createTokenRequest": {
             "type": "object",
             "properties": {
+                "is_admin": {
+                    "type": "boolean"
+                },
                 "name": {
                     "type": "string"
                 }
